2acdffcecc969db0d77bc6f8235f77fe4e986de9
ilaylyk <ilay_lyk@mail.ru>
2022-11-30 01:25:52 +0300

    updat Get

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -3,6 +3,8 @@ package com.example.servlet;
 import com.example.User;
 import com.example.Warehouse;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +13,16 @@ import java.io.IOException;
 import java.util.Set;
 
 @WebServlet("/users")
-public class GetUsersServlet extends HttpServlet{
+public class GetUsersServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
         Set<User> users = Warehouse.getInstance().getUsers();
         req.setAttribute("users", users);
-        resp.sendRedirect("/users");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/users.jsp");
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
